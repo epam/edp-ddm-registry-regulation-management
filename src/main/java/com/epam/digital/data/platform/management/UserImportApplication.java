1d47f448e76f776de9d6f9753a1f13c594192621
Oleksandr Lypivets <oleksandr_lypivets@epam.com>
2022-09-05 16:32:15 +0300

    [EDP-DDM-18392] Added repository refresh scheduler
    Change-Id: Ia7694ddc080b853622cb3da6f521c66213bd685f

--- src/main/java/com/epam/digital/data/platform/management/UserImportApplication.java
+++ src/main/java/com/epam/digital/data/platform/management/UserImportApplication.java
@@ -18,8 +18,10 @@ package com.epam.digital.data.platform.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UserImportApplication {
 
 	public static void main(String[] args) {
