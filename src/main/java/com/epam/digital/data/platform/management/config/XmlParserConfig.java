faacff646ab06a6cf6a0de72d17fda96173dd66e
Dmytro Shut <dmytro_shut@epam.com>
2022-09-15 10:00:23 +0300

    [EDP-DDM-18772] move XmlParserConfig to separate bean
    Change-Id: I63a2b6addec0e954fb543845ceb7c9457deb794f

---
+++ src/main/java/com/epam/digital/data/platform/management/config/XmlParserConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.digital.data.platform.management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+@Configuration
+public class XmlParserConfig {
+
+  @Bean
+  public DocumentBuilder documentBuilder() throws ParserConfigurationException {
+    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder db = null;
+    documentBuilderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+    documentBuilderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+    documentBuilderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+    documentBuilderFactory.setExpandEntityReferences(false);
+    db = documentBuilderFactory.newDocumentBuilder();
+    return db;
+  }
+}
