2da4b5b4bcaf1fbb26334bb8475a8236eb5f0b5b
Dmytro Shut <dmytro_shut@epam.com>
2022-09-28 17:17:20 +0300

    [EDP-DDM-17388] adoc improvements
    Change-Id: If8742e7809c78d88f9a48c17d9b462a0d47a65f6

--- src/test/java/com/epam/digital/data/platform/management/controller/CandidateVersionBusinessProcessControllerTest.java
+++ src/test/java/com/epam/digital/data/platform/management/controller/CandidateVersionBusinessProcessControllerTest.java
@@ -48,13 +48,15 @@ public class CandidateVersionBusinessProcessControllerTest {
   private static final String CANDIDATE_VERSION_ID = "id1";
   private static final String BASE_REQUEST = "/versions/candidates/id1/business-processes";
   private MockMvc mockMvc;
+  private static final String BUSINESS_PROCESS_ID = "John_Does_process";
+  private static final String BUSINESS_PROCESS_NAME = "John Doe added new component";
   private static final String BUSINESS_PROCESS_CONTENT = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
       "<bpmn:definitions xmlns:bpmn=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\" xmlns:dc=\"http://www.omg.org/spec/DD/20100524/DC\" xmlns:modeler=\"http://camunda.org/schema/modeler/1.0\" id=\"Definitions_1poh5q3\" targetNamespace=\"http://bpmn.io/schema/bpmn\" exporter=\"Camunda Modeler\" exporterVersion=\"5.1.0\" modeler:executionPlatform=\"Camunda Cloud\" modeler:executionPlatformVersion=\"8.0.0\">\n" +
-      "  <bpmn:process id=\"name\" name=\"title\" isExecutable=\"true\">\n" +
+      "  <bpmn:process id=\"" + BUSINESS_PROCESS_ID + "\" name=\"" + BUSINESS_PROCESS_NAME + "\" isExecutable=\"true\">\n" +
       "    <bpmn:startEvent id=\"StartEvent_1\" />\n" +
       "  </bpmn:process>\n" +
       "  <bpmndi:BPMNDiagram id=\"BPMNDiagram_1\">\n" +
-      "    <bpmndi:BPMNPlane id=\"BPMNPlane_1\" bpmnElement=\"name\">\n" +
+      "    <bpmndi:BPMNPlane id=\"BPMNPlane_1\" bpmnElement=\"" + BUSINESS_PROCESS_ID + "\">\n" +
       "      <bpmndi:BPMNShape id=\"_BPMNShape_StartEvent_2\" bpmnElement=\"StartEvent_1\">\n" +
       "        <dc:Bounds x=\"179\" y=\"79\" width=\"36\" height=\"36\" />\n" +
       "      </bpmndi:BPMNShape>\n" +
@@ -85,15 +87,15 @@ public class CandidateVersionBusinessProcessControllerTest {
   @Test
   @SneakyThrows
   public void getBusinessProcess() {
-    Mockito.when(businessProcessService.getProcessContent("businessProcess", CANDIDATE_VERSION_ID))
+    Mockito.when(businessProcessService.getProcessContent(BUSINESS_PROCESS_ID, CANDIDATE_VERSION_ID))
         .thenReturn(BUSINESS_PROCESS_CONTENT);
 
-    mockMvc.perform(get(BASE_REQUEST + "/businessProcess"))
+    mockMvc.perform(get(BASE_REQUEST + "/" + BUSINESS_PROCESS_ID))
         .andExpectAll(
             status().isOk(),
             content().contentType("text/xml"),
-            xpath("/bpmn:definitions/bpmn:process/@id", BPMN_NAMESPACES).string("name"),
-            xpath("/bpmn:definitions/bpmn:process/@name", BPMN_NAMESPACES).string("title")
+            xpath("/bpmn:definitions/bpmn:process/@id", BPMN_NAMESPACES).string(BUSINESS_PROCESS_ID),
+            xpath("/bpmn:definitions/bpmn:process/@name", BPMN_NAMESPACES).string(BUSINESS_PROCESS_NAME)
         ).andDo(document("versions/candidates/{versionCandidateId}/business-processes/{businessProcessName}/GET"));
   }
 
@@ -102,9 +104,9 @@ public class CandidateVersionBusinessProcessControllerTest {
   public void getBusinessProcessesByVersionId() {
     Mockito.when(businessProcessService.getProcessesByVersion(CANDIDATE_VERSION_ID))
         .thenReturn(List.of(BusinessProcessResponse.builder()
-                .path("/bpmn/business-process.bpmn")
-                .name("business-process")
-                .title("title")
+                .path("/bpmn/" + BUSINESS_PROCESS_ID + ".bpmn")
+                .name(BUSINESS_PROCESS_ID)
+                .title(BUSINESS_PROCESS_NAME)
                 .created(LocalDateTime.now())
                 .updated(LocalDateTime.now())
             .build()));
@@ -121,32 +123,32 @@ public class CandidateVersionBusinessProcessControllerTest {
   @SneakyThrows
   public void createBusinessProcess() {
     mockMvc.perform(MockMvcRequestBuilders.post(
-            BASE_REQUEST + "/{businessProcessName}",  "name")
+            BASE_REQUEST + "/{businessProcessName}", BUSINESS_PROCESS_ID)
         .contentType(MediaType.TEXT_XML).content(BUSINESS_PROCESS_CONTENT)
         .accept(MediaType.TEXT_XML)).andExpectAll(
         status().isCreated(),
         content().contentType("text/xml"),
-        xpath("/bpmn:definitions/bpmn:process/@id", BPMN_NAMESPACES).string("name"),
-        xpath("/bpmn:definitions/bpmn:process/@name", BPMN_NAMESPACES).string("title"))
+        xpath("/bpmn:definitions/bpmn:process/@id", BPMN_NAMESPACES).string(BUSINESS_PROCESS_ID),
+        xpath("/bpmn:definitions/bpmn:process/@name", BPMN_NAMESPACES).string(BUSINESS_PROCESS_NAME))
         .andDo(document("versions/candidates/{versionCandidateId}/business-processes/{businessProcessName}/POST"));
 
-    Mockito.verify(businessProcessService).createProcess("name", BUSINESS_PROCESS_CONTENT, CANDIDATE_VERSION_ID);
+    Mockito.verify(businessProcessService).createProcess(BUSINESS_PROCESS_ID, BUSINESS_PROCESS_CONTENT, CANDIDATE_VERSION_ID);
   }
 
   @Test
   @SneakyThrows
   public void updateBusinessProcess() {
     mockMvc.perform(MockMvcRequestBuilders.put(
-            BASE_REQUEST + "/{businessProcessName}",  "name")
+            BASE_REQUEST + "/{businessProcessName}", BUSINESS_PROCESS_ID)
         .contentType(MediaType.TEXT_XML).content(BUSINESS_PROCESS_CONTENT)
         .accept(MediaType.TEXT_XML)).andExpectAll(
         status().isOk(),
         content().contentType("text/xml"),
-        xpath("/bpmn:definitions/bpmn:process/@id", BPMN_NAMESPACES).string("name"),
-        xpath("/bpmn:definitions/bpmn:process/@name", BPMN_NAMESPACES).string("title"))
+        xpath("/bpmn:definitions/bpmn:process/@id", BPMN_NAMESPACES).string(BUSINESS_PROCESS_ID),
+        xpath("/bpmn:definitions/bpmn:process/@name", BPMN_NAMESPACES).string(BUSINESS_PROCESS_NAME))
         .andDo(document("versions/candidates/{versionCandidateId}/business-processes/{businessProcessName}/PUT"));
 
-    Mockito.verify(businessProcessService).updateProcess(BUSINESS_PROCESS_CONTENT, "name", CANDIDATE_VERSION_ID);
+    Mockito.verify(businessProcessService).updateProcess(BUSINESS_PROCESS_CONTENT, BUSINESS_PROCESS_ID, CANDIDATE_VERSION_ID);
 
   }
 
@@ -154,10 +156,10 @@ public class CandidateVersionBusinessProcessControllerTest {
   @SneakyThrows
   public void deleteBusinessProcess() {
     mockMvc.perform(MockMvcRequestBuilders.delete(
-            BASE_REQUEST + "/{businessProcessName}",  "name"))
+            BASE_REQUEST + "/{businessProcessName}", BUSINESS_PROCESS_ID))
         .andExpect(status().isNoContent())
         .andDo(document("versions/candidates/{versionCandidateId}/business-processes/{businessProcessName}/DELETE"));
 
-    Mockito.verify(businessProcessService).deleteProcess("name", CANDIDATE_VERSION_ID);
+    Mockito.verify(businessProcessService).deleteProcess(BUSINESS_PROCESS_ID, CANDIDATE_VERSION_ID);
   }
 }
