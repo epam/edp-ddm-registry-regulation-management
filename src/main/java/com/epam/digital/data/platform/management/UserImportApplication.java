78432aefa5495696357e9aa745282389035f3e32
Eduard_Kuznetsov <eduard_kuznetsov@epam.com>
2022-10-13 15:04:32 +0300

    [EDP-DDM-20158] Added event based repository cloning and added fetching on repo after rebase
    Change-Id: I7510a05ef47946a70b4fb79f89fe91cafe7f4495

--- src/main/java/com/epam/digital/data/platform/management/UserImportApplication.java
+++ src/main/java/com/epam/digital/data/platform/management/UserImportApplication.java
@@ -21,6 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
@@ -28,6 +29,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 		"data.model.snapshot"})
 @EnableScheduling
 @EnableCaching
+@EnableAsync
 public class UserImportApplication {
 
 	public static void main(String[] args) {
