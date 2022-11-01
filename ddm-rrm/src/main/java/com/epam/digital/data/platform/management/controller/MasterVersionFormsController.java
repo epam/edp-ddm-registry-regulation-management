6162520da085a10a6025f689b79687035ffd0407
Oleksandr Lypivets <oleksandr_lypivets@epam.com>
2022-11-01 14:09:15 +0200

    [EDP-DDM-20780] Separate project on 2 modules
    Change-Id: I221a4865129b7d0992f82a047367d50068df1277

---
+++ ddm-rrm/src/main/java/com/epam/digital/data/platform/management/controller/MasterVersionFormsController.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.epam.digital.data.platform.management.controller;
+
+import com.epam.digital.data.platform.management.core.config.GerritPropertiesConfig;
+import com.epam.digital.data.platform.management.model.dto.FormDetailsShort;
+import com.epam.digital.data.platform.management.model.exception.DetailedErrorResponse;
+import com.epam.digital.data.platform.management.service.FormService;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Registry regulations Master version Forms management Rest API")
+@RestController
+@RequestMapping("/versions/master/forms")
+@RequiredArgsConstructor
+public class MasterVersionFormsController {
+
+  private final FormService formService;
+  private final GerritPropertiesConfig gerritPropertiesConfig;
+
+  @Operation(description = "Get lest of forms for master version", parameters = {
+      @Parameter(in = ParameterIn.HEADER,
+          name = "X-Access-Token",
+          description = "Token used for endpoint security",
+          required = true,
+          schema = @Schema(type = "string"))},
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "OK",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  array = @ArraySchema(schema = @Schema(implementation = FormDetailsShort.class)))),
+          @ApiResponse(responseCode = "401",
+              description = "Unauthorized",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+          @ApiResponse(responseCode = "403",
+              description = "Forbidden",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = DetailedErrorResponse.class)))})
+  @GetMapping
+  public ResponseEntity<List<FormDetailsShort>> getFormsFromMaster()
+      throws IOException, RestApiException {
+    var masterVersionId = gerritPropertiesConfig.getHeadBranch();
+    return ResponseEntity.ok().body(formService.getFormListByVersion(masterVersionId).stream()
+        .map(e -> FormDetailsShort.builder()
+            .name(e.getName())
+            .title(e.getTitle())
+            .created(e.getCreated())
+            .updated(e.getUpdated())
+            .build())
+        .collect(Collectors.toList()));
+  }
+
+  @Operation(description = "Get specific form full details",
+      parameters = @Parameter(in = ParameterIn.HEADER,
+          name = "X-Access-Token",
+          description = "Token used for endpoint security",
+          required = true,
+          schema = @Schema(type = "string")),
+      responses = {
+          @ApiResponse(responseCode = "200",
+              description = "OK",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = Map.class))),
+          @ApiResponse(responseCode = "401",
+              description = "Unauthorized",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+          @ApiResponse(responseCode = "403",
+              description = "Forbidden",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)),
+          @ApiResponse(responseCode = "404",
+              description = "Not Found",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = DetailedErrorResponse.class))),
+          @ApiResponse(responseCode = "500",
+              description = "Internal server error",
+              content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                  schema = @Schema(implementation = DetailedErrorResponse.class)))})
+  @GetMapping("/{formName}")
+  public ResponseEntity<Object> getForm(@PathVariable @Parameter(description = "Form name", required = true) String formName)
+      throws IOException {
+    var masterVersionId = gerritPropertiesConfig.getHeadBranch();
+    return ResponseEntity.ok()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(formService.getFormContent(formName, masterVersionId));
+  }
+}
---
+++ ddm-rrm/src/test/java/com/epam/digital/data/platform/management/controller/MasterVersionControllerTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2022 EPAM Systems.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.epam.digital.data.platform.management.controller;
+
+import static com.epam.digital.data.platform.management.controller.CandidateVersionControllerTest.VERSION_CANDIDATE_AUTHOR;
+import static com.epam.digital.data.platform.management.controller.CandidateVersionControllerTest.VERSION_CANDIDATE_DESCRIPTION;
+import static com.epam.digital.data.platform.management.controller.CandidateVersionControllerTest.VERSION_CANDIDATE_NAME;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.epam.digital.data.platform.management.core.config.GerritPropertiesConfig;
+import com.epam.digital.data.platform.management.model.dto.ChangeInfoDetailedDto;
+import com.epam.digital.data.platform.management.service.VersionManagementService;
+import java.time.LocalDateTime;
+
+import com.epam.digital.data.platform.management.service.impl.GlobalSettingServiceImpl;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ControllerTest(MasterVersionController.class)
+class MasterVersionControllerTest {
+
+  static final String BASE_URL = "/versions/master";
+
+  MockMvc mockMvc;
+  @MockBean
+  private GerritPropertiesConfig gerritPropertiesConfig;
+  @MockBean
+  private VersionManagementService versionManagementService;
+  @MockBean
+  private GlobalSettingServiceImpl globalSettingServiceImpl;
+  @RegisterExtension
+  final RestDocumentationExtension restDocumentation = new RestDocumentationExtension();
+
+  @BeforeEach
+  public void setUp(WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation) {
+
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation)).build();
+  }
+
+  @Test
+  @SneakyThrows
+  void getMaster() {
+    var expectedChangeInfo = ChangeInfoDetailedDto.builder()
+        .number(1)
+        .owner(VERSION_CANDIDATE_AUTHOR)
+        .description(VERSION_CANDIDATE_DESCRIPTION)
+        .subject(VERSION_CANDIDATE_NAME)
+        .submitted(LocalDateTime.of(2022, 7, 29, 12, 31))
+        .build();
+    Mockito.when(versionManagementService.getMasterInfo()).thenReturn(expectedChangeInfo);
+
+    mockMvc.perform(get(BASE_URL))
+        .andExpectAll(
+            status().isOk(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            jsonPath("$.id", is("1")),
+            jsonPath("$.name", is(VERSION_CANDIDATE_NAME)),
+            jsonPath("$.description", is(VERSION_CANDIDATE_DESCRIPTION)),
+            jsonPath("$.author", is(VERSION_CANDIDATE_AUTHOR)),
+            jsonPath("$.latestUpdate", is("2022-07-29T12:31:00.000Z")),
+            jsonPath("$.published", nullValue()),
+            jsonPath("$.inspector", nullValue()),
+            jsonPath("$.validations", nullValue()))
+        .andDo(document("versions/master/GET"));
+    Mockito.verify(versionManagementService).getMasterInfo();
+  }
+
+  @Test
+  @SneakyThrows
+  void getMasterNoLastVersions() {
+    Mockito.when(versionManagementService.getMasterInfo()).thenReturn(null);
+
+    mockMvc.perform(get(BASE_URL))
+        .andExpectAll(
+            status().isOk(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            jsonPath("$.id", nullValue()),
+            jsonPath("$.name", nullValue()),
+            jsonPath("$.description", nullValue()),
+            jsonPath("$.author", nullValue()),
+            jsonPath("$.latestUpdate", nullValue()),
+            jsonPath("$.published", nullValue()),
+            jsonPath("$.inspector", nullValue()),
+            jsonPath("$.validations", nullValue()))
+        .andDo(document("versions/master/GET"));
+    Mockito.verify(versionManagementService).getMasterInfo();
+  }
+}
